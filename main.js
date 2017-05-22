a4960562d57773b465aaa3bfb9b2647b5f646c18
Demian88 <vomitcaballero@yandex.ru>
2017-05-22 22:46:42 +0300

    Build

--- main.js
+++ main.js
@@ -15,9 +15,7 @@ function createWindow() {
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file',
     slashes: true,
-  }));
-
-  win.webContents.openDevTools();
+  }))
 
   win.on('closed', () => {
     win = null;
